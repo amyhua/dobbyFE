e75a7e6fcbaf6981dd24f505f8caa7d9b667be77
Amy Hua <foramyhua@gmail.com>
2014-05-18 10:27:14 -0700

    WIP: Profile fields populated

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -22,4 +22,27 @@ app
     }
   };
 })
+.filter('trim', function() {
+  return function(string) {
+    return string.slice(0,1);
+  };
+})
+.filter('roundRating', function() {
+  return function(number) {
+
+    if (number === undefined) return 0;
+
+    number = Number(number);
+    var floor = Math.floor(number);
+    var remainder = number % 1;
+
+    if (remainder < 0.25) {
+      return floor;
+    } else if (remainder <= 0.5) {
+      return floor + 0.5;
+    } else {
+      return floor + 1;
+    }
+  };
+})
 
--- app/assets/javascripts/profileCtrl.js
+++ app/assets/javascripts/profileCtrl.js
@@ -2,4 +2,32 @@ app.controller('ProfileCtrl', function($scope, $location, $http, $timeout, cfpLo
   console.log('ProfileCtrl', $routeParams);
 
   // TODO
+
+  $scope.jobID = $routeParams['job_id'];
+  $scope.profileID = $routeParams['profile_id'];
+  $scope.jobOfferPrice = $routeParams['job_offer_price'];
+
+  $scope.profile = {
+    firstName: 'Amy',
+    lastName: 'Hua',
+    overallRating: 4.32,
+    numRatingsOverall: 30,
+    numRatingsFromCustomers: 0,
+    numRatingsFromDobbies: 30,
+    city: 'Seattle',
+    state: 'WA',
+    verified: 'Yes',
+    bgChecked: 'No',
+    aboutMe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    imageUrl: 'https://avatars0.githubusercontent.com/u/3581705?s=460'
+  }
+
+  // $http.get('/api/profile/:profile_id').success(function(profile){
+
+  // })
+
+
+  $scope.goBack = function(){
+    $location.path('/bidded_dobbies_list')
+  }
 });
