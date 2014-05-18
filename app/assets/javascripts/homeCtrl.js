fe46bed0df96f9a890da22a732e1d5573cae03fe
Amy Hua <foramyhua@gmail.com>
2014-05-17 19:07:39 -0700

    Updated pages and precompiled assets

--- app/assets/javascripts/homeCtrl.js
+++ app/assets/javascripts/homeCtrl.js
@@ -24,9 +24,13 @@ app.controller('HomeCtrl', function($scope, $location, jobProperties){
 app.controller('JobCtrl', function($scope, $location, jobProperties){
   console.log('JobCtrl');
 
-  $scope.urgencyTypes = [ 'ASAP', '2 hours', '24 hours'];
+  $scope.urgencyTypes = [ 'ASAP', 'within 2 hours', 'within 24 hours'];
+
+  $scope.jobUrgency = 'ASAP';
 
   $scope.jobString = jobProperties.getJobString();
+
+  $scope.jobDetail = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
   $scope.goBack = function(path){
     $location.path(path);
